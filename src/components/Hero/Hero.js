8b105d022f52768544dd9d4c070405d5219d5105
Scovia Achan <achanscovia@gmail.com>
2021-09-02 08:37:58 +0300

    fix: change button text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  
   <>
     <Section row nopadding>
       <LeftSection>
@@ -15,9 +18,12 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        This portfolio site shows all the skills I have attained and also showcases the projects I have worked on so far.
+          This portfolio site shows all the skills I have attained and also
+          showcases the projects I have worked on so far.
         </SectionText>
-        <a href="https://drive.google.com/file/d/1msr8694ZAZnwGAs1R3ksgQoPj7Pt9D4O/view?usp=sharing"><Button>Learn More</Button></a>
+        <a href="https://drive.google.com/file/d/1msr8694ZAZnwGAs1R3ksgQoPj7Pt9D4O/view?usp=sharing">
+          <Button>View CV</Button>
+        </a>
       </LeftSection>
     </Section>
   </>
